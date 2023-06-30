ad41171bcfa3f1d60e4db28f7e12c8a8e8c25257
Emanuel Guaymás <107131508+Dexsaah@users.noreply.github.com>
2023-06-30 15:17:08 -0300

    sigup back (#1)

---
+++ controllers/users/register.js
@@ -0,0 +1,20 @@
+import User from "../../models/User.js";
+
+export default async (req, res) => {
+  try {
+    const one = await User.create(req.body);
+
+    return res.status(201).json({
+      response: one,
+      success: true,
+      message: "User created",
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      response: null,
+      success: false,
+      message: error.message,
+    });
+  }
+};
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
 import Router from 'express';
 import read from '../controllers/users/read.js'
+import create from '../controllers/users/register.js'
 
 let auth_router = Router();
 
-auth_router.get('/', read) //leer uno o todos
-
+auth_router.get('/', read);
+auth_router.post('/signup', create);
 
 export default auth_router;
