3d73e2573a60b954911e69357eb8eb65a10429de
Jose Javier Valdez <133820929+valdeveloper@users.noreply.github.com>
2023-07-05 13:24:34 -0400

    M04 valid (#5)

--- schemas/chapter/register.js
+++ schemas/chapter/register.js
@@ -2,17 +2,43 @@ import joi from 'joi'
 
 const chapterRegister = joi.object({
     manga_id: joi.string()
-        .required(),
+        .required()
+        .min(3)
+        .messages({
+            'any.required' : 'The manga ID is required',
+            'string.min': 'The manga ID must have a minimun of 3 characters',
+            'string.alphanum': 'Manga ID must be an alphanumeric character.',
+        }),
     title: joi.string()
         .required()
         .min(1)
-        .max(30),
+        .max(30)
+        .messages({
+            'any.required' : 'Title is required',
+            'string.min': 'Title must have a minimun of 1 character',
+            'string.max': 'Title must have a maximum of 30 characters',
+            'string.alphanum': 'Title must be an alphanumeric character.',
+        }),
     cover_photo: joi.string()
         .required()
-        .uri(),
+        .uri()
+        .messages({
+            'any.required' : 'Cover photo is required',
+            'string.uri': 'Cover photo must be a valid URL',
+        }),
     pages: joi.array()
-        .required(),
-    order: joi.number(),    
+        .required()
+        .messages({
+            'any.required' : 'Pages are required',
+            'string.min': 'Pages must be at least one valid URL',
+        }),
+    order: joi.number()
+        .integer()
+        .min(1)
+        .messages({
+        'number.integer' : 'Order must be an integer number',
+    }),
+
 
 })
 
