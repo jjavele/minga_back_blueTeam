b71f1f353a02e7af7366526af09c28dc1241a108
Jose Javier Valdez <133820929+valdeveloper@users.noreply.github.com>
2023-07-17 09:34:35 -0400

    M09 endpoint (#20)
    * M04 valid
    
    * app
    
    * add cover photo middleware done
    
    * arreglo esquema
    
    * passport
    
    * passport added
    
    * m04-next-order
    
    * little changes
    
    * conflicts resolved
    
    * endpoint completed
    
    * m09 back
    
    ---------
    
    Co-authored-by: evaaguilera <eva2aguilera0tauda01@gmail.com>

--- controllers/mangas/read_news.js
+++ controllers/mangas/read_news.js
@@ -21,18 +21,18 @@ export default async (req, res, next) => {
       return res.status(404).json({ error: 'Author not founded' })
 
     }else if (mangas.length < 4) {
+
       return res.json({ logo: 'https://i.postimg.cc/fyJsspq8/image.png', succes: true})
 
     }else if (mangas.length < 8) {
       let newestMangas = mangas.slice(0, 2);
       let oldestMangas = mangas.slice(-2);
-      return res.json({ allMangas: [...newestMangas, ...oldestMangas], succes: true });
+      return res.json({ mangas: [...newestMangas, ...oldestMangas], success: true });
 
     }else {
       let newestMangas = mangas.slice(0, 4);
       let oldestMangas = mangas.slice(-4);
-      return res.json({ new: newestMangas, old: oldestMangas, succes: true });
-
+      return res.json({ mangas: [...newestMangas, ...oldestMangas], success: true });
     }
 
   } catch (error) {
