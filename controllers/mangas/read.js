9240ca9c1cfea068a130f16c1086605dea6ef26c
Emanuel Guaymás <107131508+Dexsaah@users.noreply.github.com>
2023-07-17 13:22:22 -0300

    Emanuel10 (#22)
    * ultimo push antes de la entrega sprint 3
    
    * fix final sprint 3
    
    ---------
    
    Co-authored-by: eva-aguilera <119910190+eva-aguilera@users.noreply.github.com>

--- controllers/mangas/read.js
+++ controllers/mangas/read.js
@@ -33,13 +33,18 @@ async function read(req, res, next) {
     const prevPage = pagination.page > 1 ? Number(pagination.page) - 1 : 0;
     const nextPage = pagination.page < totalPages ? Number(pagination.page) + 1 : 0;
 
-    return res.status(200).json({
+    const respuesta = {
       success: true,
       mangas: mangas,
       prev: prevPage,
-      next: nextPage
-      
-    });
+      next: nextPage,
+      totalPages
+    }
+    console.log(respuesta)
+
+    return res.status(200).json(
+    respuesta  
+    );
   } catch (error) {
     next(error);
   }
